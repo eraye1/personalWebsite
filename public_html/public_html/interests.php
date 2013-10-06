45608eaeb0d5215a14ff10f70710f545b9d1c402
Raymond Wang <minjianwang@gmail.com>
2013-10-05 18:25:21 -0700

    First big iteration and push.  WIP Hobbies and Interests.

---
+++ public_html/public_html/interests.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Contact Raymond Wang">
+    <meta name="author" content="Raymond Minjian Wang">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+    <title>Contact Me</title>
+
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/justified-nav.css" rel="stylesheet">
+    <link href="/assets/css/grid.css" rel="stylesheet">
+</style>
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="masthead">
+
+        <ul class="nav nav-justified">
+          <li><a href="/tech.php">Tech</a></li>
+          <li><a href="/projects.php">Projects</a></li>
+          <li class="active"><a href="/interests.php">Interests</a></li>
+          <li><a href="https://dl.dropboxusercontent.com/u/41434179/WangRaymond_Resume.pdf">Resume</a>
+          <li><a href="/contact.php">Contact Me</a></li>
+          <li><a href="/index.php">Home</a></li>
+        </ul>
+      </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Interests</h1>
+        <p class="lead">The areas in Computer Science and Electrical Engineering I find intriguing.</p>
+      </div>
+
+      <hr>
+
+      <h3>Technical Skillset</h3>
+      <p>These are the current tools I've worked with.  Looking forward to learning new tools and skills.</p>
+      <div class="row">
+        <div class="col-md-4">
+          Languages
+          <div class="row">
+            <div class="col-md-3">Java</div>
+            <div class="col-md-3">C++</div>
+            <div class="col-md-3">C</div>
+            <div class="col-md-3">Python</div>
+          </div>
+        </div>
+        <div class="col-md-4">Databases
+          <div class="row">
+            <div class="col-md-4">MySQL</div>
+            <div class="col-md-4">Hibernate</div>
+            <div class="col-md-4">JDBC</div>
+          </div>
+        </div>
+       <div class="col-md-4">Mobile
+          <div class="row">
+            <div class="col-md-12">Android</div>
+          </div>
+        </div>
+      </div>
+     <div class="row">
+        <div class="col-md-4">
+          Web Dev
+          <div class="row">
+            <div class="col-md-3">HTML</div>
+            <div class="col-md-3">CSS</div>
+            <div class="col-md-3">JQuery</div>
+            <div class="col-md-3">Bootstrap</div>
+          </div>
+        </div>
+        <div class="col-md-4">Testing
+          <div class="row">
+            <div class="col-md-6">JUnit</div>
+            <div class="col-md-6">Unit Testing</div>
+          </div>
+        </div>
+       <div class="col-md-4">Networking
+          <div class="row">
+            <div class="col-md-3">TCP</div>
+            <div class="col-md-3">UDP</div>
+            <div class="col-md-3">IP</div>
+            <div class="col-md-3">Physical</div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+     <h3>Interests</h3>
+      <p>These are the current ideas I'm really excited about.</p>
+      <canvas id="viewport" width="1000" height="600"></canvas>
+
+      <!-- Site footer -->
+      <div class="footer">
+        <p>By Raymond Minjian Wang</p> <!--I want to store the last modified date in the database and query it out each time so we establish consistency across pages-->
+      </div>
+
+    </div> <!-- /container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="/assets/js/arbor.js"></script>
+    <script src="/assets/js/arbor-graphics.js"></script>
+
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
+			
